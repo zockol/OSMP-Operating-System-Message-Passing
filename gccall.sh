ea418bf1e4ecc32a5c86d724fe23b4e218ae79c1
Felix Grüning <fg518839@fh-muenster.de>
2023-05-31 04:30:46 +0200

    barrier geht

--- gccall.sh
+++ gccall.sh
@@ -2,5 +2,6 @@
 
 BASEDIR=$(dirname "$0")
 gcc $BASEDIR/OSMP/OSMPExecutable/osmpexecutable.c $BASEDIR/OSMP/OSMPLib/osmplib.c -o $BASEDIR/OSMP/OSMPExecutable/osmpexecutable
+gcc $BASEDIR/OSMP/OSMPExecutable/osmpbarriertest.c $BASEDIR/OSMP/OSMPLib/osmplib.c -o $BASEDIR/OSMP/OSMPExecutable/osmpbarrier
 gcc $BASEDIR/OSMP/OSMPStarter/osmprun.c -o $BASEDIR/osmp
 gcc $BASEDIR/Lib/Echoall/echoall.c -o $BASEDIR/Lib/Echoall/echoall
