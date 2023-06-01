cb64050c12c0429116195a0c1aed8d949c2dd8d4
Patrick Zockol <pz113634@fh-muenster.de>
2023-06-01 15:45:39 +0200

    .

--- OSMP/OSMPExecutable/osmpsendrecieve.c
+++ OSMP/OSMPExecutable/osmpsendrecieve.c
@@ -17,14 +17,18 @@ int main(int argc, char *argv[]) {
     if (size != 2) {
         exit(-1);
     }
-    if (rank == 0) { // OSMP process 0
-        bufin[0] = 4711;
-        bufin[1] = 4712;
-        bufin[2] = 4713;
-        rv = OSMP_Send(bufin, 3, OSMP_INT, 1);
+    if (rank == 0) { // OSMP process
+        for(int i = 0; i < 100; i++) {
+            bufin[0] = i+1;
+            rv = OSMP_Send(bufin, 1, OSMP_INT, 1);
+        }
+
     } else { // OSMP process 1
-        rv = OSMP_Recv(bufout, 3, OSMP_INT, &source, &len);
-        printf("OSMP process %d received %d byte from %d [%d:%d:%d] \n", rank, len, source, bufout[0], bufout[1], bufout[2]);
+        for(int i = 0; i < 100; i++) {
+            sleep(5);
+            rv = OSMP_Recv(bufout, 1, OSMP_INT, &source, &len);
+            printf("OSMP process %d received %d byte from %d [%d] \n", rank, len, source, bufout[0]);
+        }
     }
     rv = OSMP_Finalize();
     return 0;
