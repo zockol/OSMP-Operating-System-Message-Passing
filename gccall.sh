815db241f331a85df6c9a97f1780b40111287b32
Felix Grüning <fg518839@fh-muenster.de>
2023-06-05 17:00:43 +0200

    cmake

--- gccall.sh
+++ gccall.sh
@@ -1,13 +1,4 @@
 #! /bin/sh
 
-BASEDIR=$(dirname "$0")
-
-##executables
-gcc $BASEDIR/OSMP/OSMPExecutable/osmpbarriertest.c $BASEDIR/OSMP/OSMPLib/osmplib.c -o $BASEDIR/OSMP/OSMPExecutable/osmpbarrier
-gcc $BASEDIR/OSMP/OSMPExecutable/osmpsendrecieve.c $BASEDIR/OSMP/OSMPLib/osmplib.c -o $BASEDIR/OSMP/OSMPExecutable/osmpsendrecieve
-gcc $BASEDIR/OSMP/OSMPExecutable/osmpbcast.c $BASEDIR/OSMP/OSMPLib/osmplib.c -o $BASEDIR/OSMP/OSMPExecutable/osmpbcast
-
-
-
-gcc $BASEDIR/OSMP/OSMPStarter/osmprun.c -o $BASEDIR/osmp
-gcc $BASEDIR/Lib/Echoall/echoall.c -o $BASEDIR/Lib/Echoall/echoall
+cmake -B build -s .
+cmake --build build
